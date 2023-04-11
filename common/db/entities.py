6aae13b0cbb64a89af09b3425ac334776b2b0fe6
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-04-11 15:05:10 -0700

    Added entities

--- common/db/entities.py
+++ common/db/entities.py
@@ -5,3 +5,70 @@
 # user_shoutout
 
 # constructor for edges is user_shoutout(from, to)
+
+from dataclasses import dataclass
+from typing import Any, Dict, List
+import datetime
+
+@dataclass
+class User:
+    UserID: str # unique
+    Username: str # unique
+    Email: str # unique
+    UserAccessToken: str
+    DisplayName: str
+    PasswordHash: object
+    Picture: str
+
+@dataclass
+class Event:
+    EventID: str # unique
+    Title: str
+    Description: str
+    Picture: str
+    Location: str
+    StartDateTime: datetime
+    EndDateTime: datetime | None
+    Visibility: str
+    TimeCreated: datetime
+
+@dataclass
+class School:
+    SchoolID: str # unique
+    Name: str
+    Abbreviation: str
+
+@dataclass
+class Intrest:
+    InterestID: str # unique
+    Name: str
+
+@dataclass
+class user_shoutout:
+    UserID: str # from
+    EventID: str # to
+
+@dataclass
+class user_join:
+    UserID: str # from
+    EventID: str # to
+
+@dataclass
+class user_host:
+    UserID: str # from
+    EventID: str # to
+
+@dataclass
+class user_school:
+    UserID: str # from
+    SchoolID: str # to
+
+@dataclass
+class event_school:
+    EventID: str # from
+    SchoolID: str # to
+
+@dataclass
+class event_tag:
+    EventID: str # from
+    InterestID: str # to
