db1f508722604b474ad294dbf8f80404dd0d98d8
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-07-25 20:28:20 -0700

    Update testtasks.py

--- src/worker/testtasks.py
+++ src/worker/testtasks.py
@@ -5,7 +5,7 @@ async def notify_all_events_starting_soon():
     # [event_ids, event_title] with each user_ids[], user_push_tokens[] = await get_and_set_all_starting_soon_events()
 
     # for unique event_id
-    #   [all parallel] notify_push_tokens(event_tite, user_push_tokens)
+    #   [all parallel] notify_push_tokens(event_tite, [user_push_tokens, user_id])
     return 0
 
 async def notify_recommended_events():
