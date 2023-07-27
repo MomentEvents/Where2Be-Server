bf08744e3fe322c2704d2d52262fe8d17c7ce3f7
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-07-27 16:48:28 -0700

    Updated moment_neo4j.py

--- src/common/neo4j/moment_neo4j.py
+++ src/common/neo4j/moment_neo4j.py
@@ -1,4 +1,6 @@
 import os
+import time
+import logging
 from neo4j import GraphDatabase
 
 class Neo4jDriverSingleton:
@@ -8,43 +10,38 @@ class Neo4jDriverSingleton:
     def get_driver_instance():
         if Neo4jDriverSingleton._driver_instance is None:
             Neo4jDriverSingleton.create_driver_instance()
-
         return Neo4jDriverSingleton._driver_instance
 
     @staticmethod
     def create_driver_instance():
-        if(Neo4jDriverSingleton._driver_instance is not None):
-            Neo4jDriverSingleton._driver_instance.close()
         # Set the connection details for the Neo4j database
-        NEO4J_BOLT_URL = os.environ.get('NEO4J_BOLT_URL')
-        NEO4J_USERNAME = os.environ.get('NEO4J_USERNAME')
-        NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD')
+        NEO4J_BOLT_URL = os.getenv('NEO4J_BOLT_URL')
+        NEO4J_USERNAME = os.getenv('NEO4J_USERNAME')
+        NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
 
         # Create a driver for the Neo4j database
         Neo4jDriverSingleton._driver_instance = GraphDatabase.driver(
             NEO4J_BOLT_URL, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
-        
+
     @staticmethod
     def close_driver_instance():
-        if(Neo4jDriverSingleton._driver_instance is not None):
+        if Neo4jDriverSingleton._driver_instance is not None:
             Neo4jDriverSingleton._driver_instance.close()
+            Neo4jDriverSingleton._driver_instance = None
 
 def get_neo4j_session():
-    try:
-        driver = Neo4jDriverSingleton.get_driver_instance()
-        driver.verify_connectivity()
-        driver_session = driver.session()
-        print("RETURNING SESSION")
-        return driver_session
-    except:
-        Neo4jDriverSingleton.close_driver_instance()
-        print("attempting to get neo4j driver again...")
-        Neo4jDriverSingleton.create_driver_instance()
-        driver = Neo4jDriverSingleton.get_driver_instance()
-        driver.verify_connectivity()
-        driver_session = driver.session()
-        print("successfully created a new neo4j driver!")
-        return driver_session
+    attempts = 1
+    max_attempts = 5
+    while attempts < max_attempts:
+        try:
+            driver = Neo4jDriverSingleton.get_driver_instance()
+            driver.verify_connectivity()
+            return driver.session()
+        except:
+            logging.error("Unable to connect to the Neo4j database. Retrying attempt #", str(attempts))
+            Neo4jDriverSingleton.close_driver_instance()
+        attempts = attempts + 1
+    raise Exception("Unable to create a Neo4j session after ", str(max_attempts), " attempts.")
 
 def test_neo4j_health():
     try:
