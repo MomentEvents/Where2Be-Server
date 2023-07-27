040dee05106385f6107ff94ae31afccbe9aa5149
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-07-26 23:19:22 -0700

    Fixed?

--- src/common/neo4j/moment_neo4j.py
+++ src/common/neo4j/moment_neo4j.py
@@ -1,6 +1,22 @@
 import os
 from neo4j import GraphDatabase
 
+class Neo4jDriverSingleton:
+    _driver_instance = None
+
+    @staticmethod
+    def get_driver_instance():
+        if Neo4jDriverSingleton._driver_instance is None:
+            # Set the connection details for the Neo4j database
+            NEO4J_BOLT_URL = os.environ.get('NEO4J_BOLT_URL')
+            NEO4J_USERNAME = os.environ.get('NEO4J_USERNAME')
+            NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD')
+
+            # Create a driver for the Neo4j database
+            Neo4jDriverSingleton._driver_instance = GraphDatabase.driver(
+                NEO4J_BOLT_URL, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
+
+        return Neo4jDriverSingleton._driver_instance
 
 def test_neo4j_health():
     try:
@@ -12,18 +28,6 @@ def test_neo4j_health():
         return str(e)
 
 def get_neo4j_session():
-    driver = get_neo4j_driver()
+    driver = Neo4jDriverSingleton.get_driver_instance()
     driver_session = driver.session()
     return driver_session
-
-
-def get_neo4j_driver():
-    # Set the connection details for the Neo4j database
-    NEO4J_BOLT_URL = os.environ.get('NEO4J_BOLT_URL')
-    NEO4J_USERNAME = os.environ.get('NEO4J_USERNAME')
-    NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD')
-
-    # Create a driver for the Neo4j database
-    driver = GraphDatabase.driver(
-        NEO4J_BOLT_URL, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
-    return driver
