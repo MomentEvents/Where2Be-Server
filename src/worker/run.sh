b85b32282d82bb82e647ff1e0683213d4648bf98
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-07-26 18:07:41 -0700

    Update run.sh

--- src/worker/run.sh
+++ src/worker/run.sh
@@ -3,11 +3,11 @@
 # Call the test_neo4j_health() function
 python3 -c "from common.neo4j.moment_neo4j import test_neo4j_health; exit(test_neo4j_health())"
 
-# If neo4j_health is good (exit code is 0), start the api.
+# If neo4j_health is good (exit code is 0), start the worker.
 if [ $? -eq 0 ]; then
     echo "Starting Where2Be Worker"
     python3 worker/app.py
 else
-    echo "Neo4j health check failed. Not starting the API."
+    echo "Neo4j health check failed. Not starting the Worker."
     exit 1
 fi
